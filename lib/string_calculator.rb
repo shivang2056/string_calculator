6fb8951eb292db30b7a702312c11b0f797463912
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-12-27 17:26:33 +0530

    Task: Refactor

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,55 +1,44 @@
 class StringCalculator
+  class << self
+    def add(string)
+      case string
+      when String
+        string.strip!
 
-  def initialize(string = nil)
-    @string = string
-  end
+        return 0 if string.empty?
 
-  def calculate
-    case @string
-    when String
-      remove_whitespaces
-
-      return 0 if empty_string?
-
-      process_string
-    else
-      return nil
-    end
-  end
-
-  private
-
-  def remove_whitespaces
-    @string.strip!
-  end
-
-  def empty_string?
-    @string.empty?
-  end
-
-  def process_string
-    if @string.start_with?("//")
-      delimiter = extract_delimiter
-      numbers_string = extract_numbers_with_delimiter
-    else
-      delimiter = ','
-      numbers_string = @string
+        process_string(string)
+      else
+        return nil
+      end
     end
 
-    numbers_string = replace_newline_with_delimiter(numbers_string, delimiter)
+    private
 
-    numbers_string.split(delimiter).map(&:to_i).sum
-  end
+    def process_string(string)
+      if string.start_with?("//")
+        delimiter = extract_delimiter(string)
+        numbers_string = extract_numbers_with_delimiter(string)
+      else
+        delimiter = ','
+        numbers_string = string
+      end
 
-  def extract_delimiter
-    @string.match(/\/\/(.)\n/)[1]
-  end
+      numbers_string = replace_newline_with_delimiter(numbers_string, delimiter)
 
-  def extract_numbers_with_delimiter
-    @string.split(/\/\/.\n/)[1]
-  end
+      numbers_string.split(delimiter).map(&:to_i).sum
+    end
 
-  def replace_newline_with_delimiter(numbers_string, delimiter)
-    numbers_string.gsub("\n", delimiter)
+    def extract_delimiter(string)
+      string.match(/\/\/(.)\n/)[1]
+    end
+
+    def extract_numbers_with_delimiter(string)
+      string.split(/\/\/.\n/)[1]
+    end
+
+    def replace_newline_with_delimiter(numbers_string, delimiter)
+      numbers_string.gsub("\n", delimiter)
+    end
   end
 end
--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -3,26 +3,26 @@ require_relative '../lib/string_calculator'
 
 class StringCalculatorTest < Minitest::Test
 
-  def test_calculate
-    assert_nil calculate
-    assert_nil calculate(12)
+  def test_add
+    assert_nil add
+    assert_nil add(12)
 
-    assert_equal 0, calculate('')
+    assert_equal 0, add('')
 
-    assert_equal 1, calculate('1')
-    assert_equal 6, calculate('1,5')
-    assert_equal 306, calculate('1,5,4, 2,   54,   122   , 21 , 11,32,54 ')
+    assert_equal 1, add('1')
+    assert_equal 6, add('1,5')
+    assert_equal 306, add('1,5,4, 2,   54,   122   , 21 , 11,32,54 ')
 
-    assert_equal 6, calculate("1\n2,3")
-    assert_equal 274, calculate("1,5\n4, 2,   54\n   122   , 21 \n 11,54 ")
+    assert_equal 6, add("1\n2,3")
+    assert_equal 274, add("1,5\n4, 2,   54\n   122   , 21 \n 11,54 ")
 
-    assert_equal 3, calculate("//;\n1;2")
-    assert_equal 301, calculate("//:\n1:4: 2:   54\n   122   : 21 : 11 \n32:54 ")
+    assert_equal 3, add("//;\n1;2")
+    assert_equal 301, add("//:\n1:4: 2:   54\n   122   : 21 : 11 \n32:54 ")
   end
 
   private
 
-  def calculate(string = nil)
-    StringCalculator.new(string).calculate
+  def add(string = nil)
+    StringCalculator.add(string)
   end
 end
